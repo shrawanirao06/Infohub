20eb59b0f3baa239fd7c7a91ef0ba474e272080e
shrawanirao06 <shrawanirao06@gmail.com>
2025-11-05 15:55:39 +0530

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ContentBodyComponent } from './content-body/content-body.component';
+import { HomeComponent } from './home/home.component';
+import { BodyComponent } from './body/body.component';
+import { FrontendTopicsComponent } from './frontend-topics/frontend-topics.component';
+import { AngularComponent } from './FrontendTopics/angular/angular.component';
+import { HtmlComponent } from './FrontendTopics/html/html.component';
+import { CssComponent } from './FrontendTopics/css/css.component';
+import { BootstrapComponent } from './FrontendTopics/bootstrap/bootstrap.component';
+import { JavascriptComponent } from './FrontendTopics/javascript/javascript.component';
+import { ReactComponent } from './FrontendTopics/react/react.component';
+import { BackendTopicsComponent } from './backend-topics/backend-topics.component';
+import { WebapiComponent } from './BackendTopics/webapi/webapi.component';
+import { DatabaseComponent } from './BackendTopics/database/database.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ParentOnchangeComponent } from './life-cycle-hooks/parent-onchange/parent-onchange.component';
+import { ChildOnchangeComponent } from './life-cycle-hooks/child-onchange/child-onchange.component';
+import { LibrariesTopicComponent } from './libraries-topic/libraries-topic.component';
+import { AOSComponent } from './Libraries-Topics/aos/aos.component';
+import { PrimeNgComponent } from './Libraries-Topics/prime-ng/prime-ng.component';
+import { MatterJsComponent } from './Libraries-Topics/matter-js/matter-js.component';
+import { G2PlotComponent } from './Libraries-Topics/g2-plot/g2-plot.component';
+import { ThreeJsComponent } from './Libraries-Topics/three-js/three-js.component';
+import { GASPComponent } from './Libraries-Topics/gasp/gasp.component';
+import { PlacementPrepComponent } from './placement-prep/placement-prep.component';
+import { CloudComputingComponent } from './cloud-computing/cloud-computing.component';
+import { MongoDBComponent } from './BackendTopics/mongo-db/mongo-db.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogFormComponent } from './blog-form/blog-form.component';
+
+const routes: Routes = [
+  {path: '', component: BodyComponent},
+  {path: 'content', component: ContentBodyComponent},
+  {path: 'frontend', component: FrontendTopicsComponent},
+  {path: 'frontend/angular', component: AngularComponent},
+  {path: 'frontend/html', component: HtmlComponent},
+  {path: 'frontend/css', component: CssComponent},
+  {path: 'frontend/bootstrap', component: BootstrapComponent},
+  {path: 'frontend/javascript', component: JavascriptComponent},
+  {path: 'frontend/react', component: ReactComponent},
+  {path: 'backend', component: BackendTopicsComponent},
+  {path: 'backend/webapi', component: WebapiComponent},
+  {path: 'backend/database', component: DatabaseComponent},
+  {path: 'contactus', component: ContactusComponent},
+  {path: 'parent', component: ParentOnchangeComponent},
+  {path: 'child', component: ChildOnchangeComponent},
+  {path: 'librariestopics', component: LibrariesTopicComponent},
+  {path: 'librariestopics/aos', component: AOSComponent},
+  {path: 'librariestopics/primeng', component: PrimeNgComponent},
+  {path: 'librariestopics/matterjs', component: MatterJsComponent},
+  {path: 'librariestopics/g2plot', component: G2PlotComponent},
+  {path: 'librariestopics/threejs', component: ThreeJsComponent},
+  {path: 'librariestopics/gasp', component: GASPComponent},
+  {path: 'placementprep' , component: PlacementPrepComponent},
+  {path: 'cloudcomputing' , component: CloudComputingComponent},
+  {path: 'backend/mongodb' , component: MongoDBComponent},
+  {path: 'blogs' , component: BlogsComponent},
+  {path: 'form' , component: BlogFormComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app.module.ts
@@ -0,0 +1,96 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { BodyComponent } from './body/body.component';
+import { HomeComponent } from './home/home.component';
+import { ContentBodyComponent } from './content-body/content-body.component';
+import { FrontendTopicsComponent } from './frontend-topics/frontend-topics.component';
+import { HtmlComponent } from './FrontendTopics/html/html.component';
+import { AngularComponent } from './FrontendTopics/angular/angular.component';
+import { ReactComponent } from './FrontendTopics/react/react.component';
+import { JavascriptComponent } from './FrontendTopics/javascript/javascript.component';
+import { CssComponent } from './FrontendTopics/css/css.component';
+import { BootstrapComponent } from './FrontendTopics/bootstrap/bootstrap.component';
+import { BackendTopicsComponent } from './backend-topics/backend-topics.component';
+import { WebapiComponent } from './BackendTopics/webapi/webapi.component';
+import { DatabaseComponent } from './BackendTopics/database/database.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ParentOnchangeComponent } from './life-cycle-hooks/parent-onchange/parent-onchange.component';
+import { ChildOnchangeComponent } from './life-cycle-hooks/child-onchange/child-onchange.component';
+import { ContactPopupComponent } from './Popup-Components/contact-popup/contact-popup.component';
+import { LibrariesTopicComponent } from './libraries-topic/libraries-topic.component';
+import { AOSComponent } from './Libraries-Topics/aos/aos.component';
+import { PrimeNgComponent } from './Libraries-Topics/prime-ng/prime-ng.component';
+import { MatterJsComponent } from './Libraries-Topics/matter-js/matter-js.component';
+import { G2PlotComponent } from './Libraries-Topics/g2-plot/g2-plot.component';
+import { ThreeJsComponent } from './Libraries-Topics/three-js/three-js.component';
+import { GASPComponent } from './Libraries-Topics/gasp/gasp.component';
+import { PlacementPrepComponent } from './placement-prep/placement-prep.component';
+import { DefaultComponent } from './default/default.component';
+import { CloudComputingComponent } from './cloud-computing/cloud-computing.component';
+import { MongoDBComponent } from './BackendTopics/mongo-db/mongo-db.component';
+import { FeedbackPopupComponent } from './feedback-popup/feedback-popup.component';
+import { FooterComponent } from './footer/footer.component';
+import { TempComponent } from './temp/temp.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogFormComponent } from './blog-form/blog-form.component';
+import { FormsModule } from '@angular/forms';
+import { BlogConfirmationDialogComponent } from './blog-confirmation-dialog/blog-confirmation-dialog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent,
+    BodyComponent,
+    HomeComponent,
+    ContentBodyComponent,
+    FrontendTopicsComponent,
+    HtmlComponent,
+    AngularComponent,
+    ReactComponent,
+    JavascriptComponent,
+    CssComponent,
+    BootstrapComponent,
+    BackendTopicsComponent,
+    WebapiComponent,
+    DatabaseComponent,
+    ContactusComponent,
+    ParentOnchangeComponent,
+    ChildOnchangeComponent,
+    ContactPopupComponent,
+    LibrariesTopicComponent,
+    AOSComponent,
+    PrimeNgComponent,
+    MatterJsComponent,
+    G2PlotComponent,
+    ThreeJsComponent,
+    GASPComponent,
+    PlacementPrepComponent,
+    DefaultComponent,
+    CloudComputingComponent,
+    MongoDBComponent,
+    FeedbackPopupComponent,
+    FooterComponent,
+    TempComponent,
+    BlogsComponent,
+    BlogFormComponent,
+    BlogConfirmationDialogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
